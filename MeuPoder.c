db8b3ea14edd5907d2d902f1cef632979d8a6359
primary157 <victorgvbh@gmail.com>
2016-08-25 15:55:55 -0300

    sectioning in functions

--- MeuPoder.c
+++ MeuPoder.c
@@ -1,60 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct 
-{	
+typedef struct {	
 	int x, y;	
-} position;
+} Position;
 
-void print_frame()
-{
+void print_frame(Position player){
         for(int i = 0; i < 7; i++)
         {
             for(int j = 0; j < 7; j++){
-                if(position.x == i && position.y == j)
+                if(player.x == i && player.y == j){
                     printf("o\t");
-                else
+		}
+                else{
                     printf("x\t");
+		}
             }
             printf("\n");
         }	
 }
-
-int main()
-{
-	FILE *save;
-	char key;
-
-	if((save = fopen("save.dat","r")) == NULL)
-		position.x = position.y = 5;
-	else
-	{
-		fscanf(save,"%d %d", &position.x, &position.y );
-		fclose(save);
+void init_Position(Position* position){
+	position->x = position->y = 5;
+}
+int save_pos(FILE* save,char mode, int* x, int* y){
+	if((save = fopen("save.dat",&mode)) == NULL){
+		return 0;
 	}
-	printf("Press Q to close and save the program, or E to close without saving.\n Press W,S,A,D to move the position!\n");
+	else{
+		switch(mode){
+			case 'r':
+				fscanf(save,"%d %d", x, y);
+				break;
+			case 'w':
+				fprintf(save,"%d %d", *x, *y);
+		}
+		fclose(save);
+		return 1;
+	}
+}
+int game_loop(Position* position){
+	char key;
 	do{
-		print_frame();
+		print_frame(*position);
 		scanf("%c",&key);
 		getchar();          //n sei pq isso existe!!
-		if((key == 'a') && (position.y > 0))
-		    position.y = position.y - 1;
+		if((key == 'a') && (position->y > 0))
+		    position->y = position->y - 1;
 		else
-		    if((key == 'd')  && (position.y < 6))
-			position.y = position.y + 1;
+		    if((key == 'd')  && (position->y < 6))
+			position->y = position->y + 1;
 		    else
-			if((key == 'w')  && (position.x > 0))
-			    position.x = position.x - 1;
+			if((key == 'w')  && (position->x > 0))
+			    position->x = position->x - 1;
 			else
-			    if((key == 's')  && (position.x < 6))
-				position.x = position.x + 1;
+			    if((key == 's')  && (position->x < 6))
+				position->x = position->x + 1;
 	}while ((key != 'q') && (key != 'e'));
-	if(key == 'q')
-	{
-		if((save = fopen("save.dat","w+")) == NULL)
-			printf("Failed during game save.");
-		else
-			fprintf(save, "%d %d", position.x, position.y);
-		fclose(save);
+	return key;
+}
+int main(){
+	Position position;
+	FILE *save = NULL;
+	
+	if(!save_pos(save, 'r',&position.x,&position.y)){
+		init_Position(&position);
+	}
+	printf("Press Q to close and save the program, or E to close without saving.\n Press W,S,A,D to move the position!\n");
+
+	if(game_loop(&position) == 'q'){
+		save_pos(save,'w',&position.x,&position.y);
 	}
 	return 0;
 }
